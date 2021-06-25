ecaad97bfbf2b94c30ae7d4e18daa3412dce8813
thinkholic <mailtokasun@gmail.com>
2021-06-25 20:03:23 +0530

    Secure endpoints for various attacks

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import path from "path";
+import helmet from "helmet";
+import xssClean from "xss-clean";
+import rateLimit from "express-rate-limit";
+import mongoSanitize from "express-mongo-sanitize";
+import hpp from "hpp";
 
 import apiRoutes from "./routes/api";
 import coreRoutes from "./routes/core";
@@ -9,7 +14,18 @@ import coreRoutes from "./routes/core";
 const app = express();
 
 app.use(cors());
-app.use(express.urlencoded({ extended: true }))
+app.use(helmet());
+app.use(xssClean());
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100, // 100 requests per IP
+});
+app.use(limiter);
+app.use(mongoSanitize());
+app.use(hpp());
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 
--- routes/core.js
+++ routes/core.js
@@ -6,4 +6,21 @@ router.get("/", function (req, res) {
   return res.send("hello world");
 });
 
+// resource
+router.get("/resource", function (req, res) {
+  return res.send("get resource");
+});
+
+router.post("/resource", function (req, res) {
+  return res.send("post resource");
+});
+
+router.put("/resource", function (req, res) {
+  return res.send("put resource");
+});
+
+router.delete("/resource", function (req, res) {
+  return res.send("delete resource");
+});
+
 export default router;
