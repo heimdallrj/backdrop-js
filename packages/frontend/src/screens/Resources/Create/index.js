3fb5e8dc1b78a66bcb0cb55715f3ac3d96f2db1e
heimdallrhg <heimdallrhg@gmail.com>
2021-07-20 14:03:05 +0530

    Add protected flag

--- packages/frontend/src/screens/Resources/Create/index.js
+++ packages/frontend/src/screens/Resources/Create/index.js
@@ -39,6 +39,8 @@ const methodsOptions = [
 
 const initialValues = {
   namespace: 'api',
+  private: false,
+  protected: false,
   name: '',
   type: 'default',
   methods: '',
@@ -114,8 +116,8 @@ export default function CreateResource() {
                     value={
                       typeOptions
                         ? typeOptions.find(
-                            (option) => option.value === values.type
-                          )
+                          (option) => option.value === values.type
+                        )
                         : ''
                     }
                     onChange={(option) => setFieldValue('type', option.value)}
@@ -163,8 +165,8 @@ export default function CreateResource() {
                     value={
                       statusOptions
                         ? statusOptions.find(
-                            (option) => option.value === values.status
-                          )
+                          (option) => option.value === values.status
+                        )
                         : ''
                     }
                     onChange={(option) => setFieldValue('status', option.value)}
