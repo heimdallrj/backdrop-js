d66730f036e51440d1c3c23f9bbf048478d380e4
heimdallrhg <heimdallrhg@gmail.com>
2021-07-23 23:19:59 +0530

    Update styles for user section

--- packages/frontend/src/screens/Users/index.js
+++ packages/frontend/src/screens/Users/index.js
@@ -1,33 +1,41 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
 
 import { fetchAll as acFetchAllUsers } from 'store/reducers/userSlice';
 
+import Table from 'components/Table';
+
 import Layout from 'components/Layout';
 
 import {
   Wrapper,
-  Heading,
   Button,
-  TableWrap,
-  Table,
-  TableHead,
-  Row,
-  ColHead,
-  TableBody,
-  Col,
   DocIcon,
   DeleteIcon,
-  ActionWrap,
+  KeyIcon,
+  FlexIcons,
   Status,
 } from './styled';
+
+const columns = [
+  { label: 'falgs', size: 5, visible: false },
+  { label: 'ID', size: 3, align: 'center' },
+  { label: 'Screen Name', size: 20 },
+  { label: 'User Name', size: 20 },
+  { label: 'e-mail', size: 20 },
+  { label: 'user role', size: 17 },
+  { label: 'status', size: 10 },
+  { label: 'actions', size: 5, visible: false },
+];
 
 export default function Users() {
   const history = useHistory();
   const dispatch = useDispatch();
 
   const { users } = useSelector((state) => state.users);
+
+  const [rows, setRows] = useState([]);
 
   const fetchAllUsers = () => dispatch(acFetchAllUsers());
 
@@ -36,60 +44,50 @@ export default function Users() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    const rowsFiltered = users.map(
+      ({ _id, screenName, userName, email, userRole, status }, index) => {
+        return {
+          id: _id,
+          data: [
+            {
+              value: <FlexIcons>{userRole === 0 && <KeyIcon />}</FlexIcons>,
+              align: 'center',
+            },
+            { value: index + 1, align: 'center' },
+            { value: screenName },
+            { value: userName },
+            { value: email },
+            { value: userRole },
+            { value: <Status>active</Status>, align: 'center' },
+            {
+              value: (
+                <FlexIcons>
+                  <DocIcon onClick={onClickUserHandler.bind(null, _id)} />
+                  <DeleteIcon onClick={() => {}} />
+                </FlexIcons>
+              ),
+            },
+          ],
+        };
+      }
+    );
+    setRows(rowsFiltered);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [users]);
+
   const onClickUserHandler = (id) => {
     history.push(`/users/${id}`);
   };
 
   return (
-    <Layout>
+    <Layout title="Users">
       <Wrapper>
-        <Heading>Users</Heading>
-
         <Link to={`/users/add`}>
-          <Button>Create a new resource</Button>
+          <Button>Create a new user</Button>
         </Link>
 
-        <TableWrap>
-          <Table>
-            <TableHead>
-              <Row>
-                <ColHead scope="col">ID</ColHead>
-                <ColHead scope="col">First Name</ColHead>
-                <ColHead scope="col" className="text-left">
-                  Last Name
-                </ColHead>
-                <ColHead scope="col" className="text-left">
-                  E-mail
-                </ColHead>
-                <ColHead scope="col" className="text-left">
-                  Status
-                </ColHead>
-                <ColHead scope="col">
-                  <span className="sr-only">Action</span>
-                </ColHead>
-              </Row>
-            </TableHead>
-            <TableBody>
-              {users.map(({ _id, firstName, lastName, email }, index) => (
-                <Row key={_id}>
-                  <Col className="text-center">{index + 1}</Col>
-                  <Col className="text-center">{firstName}</Col>
-                  <Col>{lastName}</Col>
-                  <Col>{email}</Col>
-                  <Col>
-                    <Status>active</Status>
-                  </Col>
-                  <Col>
-                    <ActionWrap>
-                      <DocIcon onClick={onClickUserHandler.bind(null, _id)} />{' '}
-                      <DeleteIcon />
-                    </ActionWrap>
-                  </Col>
-                </Row>
-              ))}
-            </TableBody>
-          </Table>
-        </TableWrap>
+        <Table columns={columns} rows={rows} />
       </Wrapper>
     </Layout>
   );
--- packages/frontend/src/screens/Users/styled.js
+++ packages/frontend/src/screens/Users/styled.js
@@ -2,61 +2,12 @@ import styled from 'styled-components';
 
 import DocumentIconSource from 'assets/icons/Document/Outline';
 import DeleteIconSource from 'assets/icons/Delete/Outline';
+import KeyIconSource from 'assets/icons/Key/Solid';
 
 export const Wrapper = styled.section``;
 
-export const Heading = styled.h5`
-  text-transform: uppercase;
-`;
-
 export const Button = styled.button`
   font-size: 0.8rem;
-`;
-
-export const TableWrap = styled.section`
-  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
-  overflow: hidden;
-  border-bottom-width: 1px;
-  width: 100%;
-  margin: 15px 0 0;
-`;
-
-export const Table = styled.table`
-  min-width: 100%;
-  border-collapse: collapse;
-  text-indent: 0;
-  border-color: inherit;
-`;
-
-export const TableHead = styled.thead`
-  border-bottom: 1px solid #e2d8d8;
-  background-color: rgba(249, 250, 251, 1);
-`;
-
-export const Row = styled.tr`
-  border-bottom: 1px solid #e2d8d8;
-`;
-
-export const ColHead = styled.th`
-  color: rgba(107, 114, 128, 1);
-  letter-spacing: 0.05em;
-  text-transform: uppercase;
-  font-weight: 500;
-  font-size: 0.75rem;
-  line-height: 1rem;
-  padding: 0.75rem;
-  width: ${(props) => `${props.width || 0}`};
-`;
-
-export const TableBody = styled.tbody`
-  background-color: rgba(255, 255, 255, 1);
-`;
-
-export const Col = styled.td`
-  white-space: nowrap;
-  padding: 0.5rem;
-  font-weight: normal;
-  font-size: 0.9rem;
 `;
 
 export const DocIcon = styled(DocumentIconSource)`
@@ -73,8 +24,16 @@ export const DeleteIcon = styled(DeleteIconSource)`
   cursor: pointer;
 `;
 
-export const ActionWrap = styled.div`
+export const KeyIcon = styled(KeyIconSource)`
+  color: gray;
+  width: 12px;
+  align-self: center;
+  cursor: pointer;
+`;
+
+export const FlexIcons = styled.div`
   display: flex;
+  flex-direction: row;
   justify-content: space-evenly;
 `;
 
