f7a75679b29eb72e3d00b53b23d155c42435e954
heimdallrhg <heimdallrhg@gmail.com>
2021-07-28 21:48:45 +0530

    Fetch data based on relationships

--- packages/server-nodejs/src/handlers/api/getSingle.js
+++ packages/server-nodejs/src/handlers/api/getSingle.js
@@ -1,18 +1,40 @@
+import forEach from 'lodash/forEach';
+
 import { db } from 'utils/database/jsondb';
 import { response } from 'utils/http';
 import * as customHanlders from 'handlers/custom';
 
 export default function getSingle(req, res) {
-  const { resourceConfig } = req;
+  try {
+    const { resourceConfig } = req;
 
-  if (resourceConfig.type === 'custom') {
-    return customHanlders[resourceConfig.type](req, res);
+    if (resourceConfig.type === 'custom') {
+      return customHanlders[resourceConfig.type](req, res);
+    }
+
+    const collName = `_${resourceConfig.name}`;
+
+    const doc = db[collName].findOne({ _id: req.params.id });
+    if (!doc) return response.notFound(res);
+
+    // Process relationship configs
+    const { schema } = resourceConfig;
+
+    forEach(schema, (val, key) => {
+      if (val.relationship) {
+        let docs = [];
+
+        val.relationship.forEach(({ selector }) => {
+          const query = {};
+          query[selector] = req.params.id;
+          docs = db[`_${key}`].find(query);
+          doc[key] = docs || [];
+        });
+      }
+    });
+
+    return response.ok(res, doc);
+  } catch (err) {
+    return response.internalError(res);
   }
-
-  const collName = `_${resourceConfig.name}`;
-
-  const doc = db[collName].findOne({ _id: req.params.id });
-  if (!doc) return response.notFound(res);
-
-  return response.ok(res, doc);
 }
