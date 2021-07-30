1be0be55208c6bec6fc4cc10027c2eb119f7eaa4
heimdallrhg <heimdallrhg@gmail.com>
2021-07-30 20:07:22 +0530

    v1.0.0-alpha.4

--- packages/cli/src/config.js
+++ packages/cli/src/config.js
@@ -1,2 +1,2 @@
-export const version = '1.0.0-alpha.3';
+export const version = '1.0.0-alpha.4';
 export const releaseUrl = `https://github.com/thinkholic/backdrop-js/releases/download/v${version}/backdrop-js-${version}.zip`;
