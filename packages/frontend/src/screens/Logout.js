f0b9fa72b3c800af7463f5920a2d4efb24784bf4
thinkholic <mailtokasun@gmail.com>
2021-08-07 20:39:00 +0530

    Fix #37

--- packages/frontend/src/screens/Logout.js
+++ packages/frontend/src/screens/Logout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { logout as acLogout } from 'store/reducers/authSlice';
@@ -7,7 +7,10 @@ export default function Logout() {
   const dispatch = useDispatch();
   const logout = () => dispatch(acLogout());
 
-  logout();
+  useEffect(() => {
+    logout();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return <Redirect to="/login" />;
 }
